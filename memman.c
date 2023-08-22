2fc9c232624fd8d969c2a2a0f5662d5d31f03975
DimickBeesley <dbeesley8@gmail.com>
2023-08-22 10:15:06 -0500

    initial commit

---
+++ memman.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Function prototypes
+void someFunction();  // Example function prototype
+
+int main() {
+    // Your code here
+
+    return 0;
+}
+
+// Example function definition
+void someFunction() {
+    // Function implementation
+}
